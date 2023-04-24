2bd1d60b7765e36164a6c34ae767014a1c552e77
Raushan Kumar <priyanshu1996r@gmail.com>
2023-04-24 22:13:17 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+
+const routes: Routes = [
+  { path:'', component:HomeComponent },
+  {path:'seller-auth', component: SellerAuthComponent},
+  {path:'seller-home', component:SellerHomeComponent, canActivate:[AuthGuard]},
+  {path:'seller-add-product', component:SellerAddProductComponent, canActivate:[AuthGuard]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { product } from '../data-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http:HttpClient) { }
+  addProduct(data:product){
+   return this.http.post('http://localhost:3000/products',data)
+  }
+  productList(){
+    return this.http.get<product[]>('http://localhost:3000/products');
+  }
+
+  deleteProduct(id:number){
+   return this.http.delete(`http://localhost:3000/products/${id}`)
+  }
+}
